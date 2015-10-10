aed93addce5a0ad1a81a06c6726af5d26fcdc6f0
actepotentiel <ruefbenjamin@gmail.com>
2015-10-10 15:24:11 +0200

    mapping des routes : ajout du /client dans les routes du module playlist

--- modules/playlists/client/config/playlists.client.routes.js
+++ modules/playlists/client/config/playlists.client.routes.js
@@ -12,19 +12,19 @@ angular.module('playlists').config(['$stateProvider',
 		}).
 		state('playlists.list', {
 			url: '',
-			templateUrl: 'modules/playlists/views/list-playlists.client.view.html'
+			templateUrl: 'modules/playlists/client/views/list-playlists.client.view.html'
 		}).
 		state('playlists.create', {
 			url: '/create',
-			templateUrl: 'modules/playlists/views/create-playlist.client.view.html'
+			templateUrl: 'modules/playlists/client/views/create-playlist.client.view.html'
 		}).
 		state('playlists.view', {
 			url: '/:playlistId',
-			templateUrl: 'modules/playlists/views/view-playlist.client.view.html'
+			templateUrl: 'modules/playlists/client/views/view-playlist.client.view.html'
 		}).
 		state('playlists.edit', {
 			url: '/:playlistId/edit',
-			templateUrl: 'modules/playlists/views/edit-playlist.client.view.html'
+			templateUrl: 'modules/playlists/client/views/edit-playlist.client.view.html'
 		});
 	}
 ]);
