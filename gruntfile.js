3392abb4d45088f5f513a89af7d4269381b7b719
jeansaigne <aymerictibere@gmail.com>
2015-11-11 15:43:15 +0100

    deploy conf: start command changed on server

--- gruntfile.js
+++ gruntfile.js
@@ -227,7 +227,7 @@ module.exports = function (grunt) {
           'npm install',
           'grunt build',
           'grunt forever:vps:stop',
-          'grunt forever:vps:start'
+          'NODE_ENV=production grunt forever:vps:start'
         ].join(' && '),
         options: {
           config: 'vps'
